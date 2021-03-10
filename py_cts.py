56aba1ea401693dab7b803f1c3b04c9422546e90
sushma <sushmas6998@gmail.com>
2021-03-10 15:52:29 +0530

    my first commit

---
+++ py_cts.py
@@ -0,0 +1,28 @@
+import os 
+import time
+
+# checking for connected devices
+print("Stability test has been completed.")
+print("Android logs and Kernel log has been generated in the workspace.")
+device = os.popen("adb devices").read()
+print(device)
+
+# connect to the selected device 172.0.0.1:62001
+connect = os.popen("adb connect "+device).read()
+#print(connect)
+
+# gradle build apk and install apk
+os.system("./gradlew clean installDebug ")
+
+#testing and collecting logcat
+
+os.system("adb shell monkey -v --throttle 100 -p com.example.MyApplication 1000 > test11log.txt")
+time.sleep((1000*100)/1000)
+
+#kernel log
+os.popen("adb shell dmesg >kernel3log.txt")
+
+#run cts
+os.system("gnome-terminal --working-directory='/home/sushma/Downloads/android-cts/tools' -e 'bash -c \"./cts-tradefed run cts; exec bash\"'")
+
+

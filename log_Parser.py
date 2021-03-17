d71802ecf360d2c0963505437d057f9c6751240b
Sushma6998 <78785486+Sushma6998@users.noreply.github.com>
2021-03-17 19:30:48 +0530

    Update log_Parser.py

--- log_Parser.py
+++ log_Parser.py
@@ -6,21 +6,12 @@ for subdir,dirs,files in os.walk(mydir):
     for filename in files:
         filepath = subdir + os.sep + filename
         if filepath.endswith(".txt"):
-            '''if filename=="test11log.txt":
-                file=open(filepath,'r').read().splitlines()
-                for elements in range(0,len(file)):
-                    if "Wakeup pending" in file[elements]:
-                       
-                       print(file[elements])
-                       print(file[elements+1])
-                       print(file[elements+2])'''
-                        
-             if filename=="test11log.txt":
+            if filename=="test11log.txt":
                 file=open(filepath,'r').read().splitlines()
                 for elements in range(0,len(file)):
                     if "Caused by" in file[elements]:
-                       print("Log parser found the Exception..") 
-                       print(file[elements])
-                       print(file[elements+1])
-                       print("Exception is coming from the below specified file and line:")
-                       print(file[elements+2])
+                        print("Log parser found the Exception..") 
+                        print(file[elements])
+                        print(file[elements+1])
+                        print("Exception is coming from the below specified file and line:")
+                        print(file[elements+2])

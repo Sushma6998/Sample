ac5e1f382f123c614b590fc273a5ed84321e6730
Sushma6998 <78785486+Sushma6998@users.noreply.github.com>
2021-03-17 19:14:06 +0530

    Update log_Parser.py

--- log_Parser.py
+++ log_Parser.py
@@ -6,16 +6,16 @@ for subdir,dirs,files in os.walk(mydir):
     for filename in files:
         filepath = subdir + os.sep + filename
         if filepath.endswith(".txt"):
-            if filename=="test11log.txt":
+            '''if filename=="test11log.txt":
                 file=open(filepath,'r').read().splitlines()
                 for elements in range(0,len(file)):
                     if "Wakeup pending" in file[elements]:
                        
                        print(file[elements])
                        print(file[elements+1])
-                       print(file[elements+2])
+                       print(file[elements+2])'''
                         
-            elif filename=="test11log.txt":
+             if filename=="test11log.txt":
                 file=open(filepath,'r').read().splitlines()
                 for elements in range(0,len(file)):
                     if "Caused by" in file[elements]:

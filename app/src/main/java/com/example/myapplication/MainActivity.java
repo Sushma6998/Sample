19e1ba186dcdbcbee52911a69a58e5f7f35d2a83
sushma <sushmas6998@gmail.com>
2021-03-17 12:56:39 +0530

    second commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.myapplication;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button buttonsum;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        addListeneronButton();
+        
+    }
+    
+    public void addListeneronButton() {
+        // buttonsum= (Button) findViewById(R.id.button);
+         buttonsum.setOnClickListener(new View.OnClickListener(){
+              @Override
+              public void onClick(View view) {
+              }
+              
+          });
+              
+    }
+ } 

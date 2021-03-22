93f452c3afd052b032fecaf20bde1f87db3f3f45
Sushma6998 <78785486+Sushma6998@users.noreply.github.com>
2021-03-22 10:01:33 +0530

    Update py_cts.py

--- py_cts.py
+++ py_cts.py
@@ -16,11 +16,11 @@ os.system("./gradlew clean installDebug ")
 
 #testing and collecting logcat
 
-os.system("adb shell monkey -v --throttle 100 -p com.example.myapplication 1000 2> test11log.txt")
+os.system("adb shell monkey -v --throttle 100 -p com.example.myapplication 1000 &> test11log.txt")
 time.sleep((1000*100)/1000)
 
 #kernel log
-os.popen("adb shell dmesg >kernel3log.txt")
+os.popen("adb shell dmesg > kernel3log.txt")
 
 #run cts
 os.system("sudo /home/sushma/Downloads/android-cts/tools/cts-tradefed run cts")
